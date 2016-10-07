e694cdd138686b0434d966c9245c0bd46d53ff7d
johanclasson <johan@classon.eu>
2016-10-07 23:00:12 +0200

    Fix that log output is lost after build task is run Working VSS-extension for UpdateDatabaseWithDbUp

--- UpdateDatabaseWithDbUp/task/Update-DatabaseWithDbUp.ps1
+++ UpdateDatabaseWithDbUp/task/Update-DatabaseWithDbUp.ps1
@@ -28,6 +28,30 @@ function Install-DbUpAndGetDllPath {
 $dllPath = Install-DbUpAndGetDllPath
 Add-Type -Path $dllPath
 
+# Log output is lost after build task is run. This hack solves it.
+$sourceCode = @"
+public class VstsUpgradeLog : DbUp.Engine.Output.IUpgradeLog
+{
+    public void WriteInformation(string format, params object[] args)
+    {
+        System.Console.WriteLine(format, args);
+    }
+
+    public void WriteError(string format, params object[] args)
+    {
+        System.Console.WriteLine("##vso[task.logissue type=error;]" + string.Format(format, args));
+    }
+
+    public void WriteWarning(string format, params object[] args)
+    {
+        System.Console.WriteLine("##vso[task.logissue type=warning;]" + string.Format(format, args));
+    }
+}
+"@
+if (-not ([System.Management.Automation.PSTypeName]'VstsUpgradeLog').Type) {
+    Add-Type -TypeDefinition $sourceCode -Language CSharp -ReferencedAssemblies $dllPath
+}
+
 $filterFunc = {
     param([string]$file)
     return $file -match $Filter
@@ -37,11 +61,20 @@ $dbUp = [DbUp.DeployChanges]::To
 $dbUp = [SqlServerExtensions]::SqlDatabase($dbUp, $ConnectionString)
 $dbUp = [StandardExtensions]::WithScriptsFromFileSystem($dbUp, $ScriptPath, $filterFunc)
 $dbUp = [StandardExtensions]::WithTransactionPerScript($dbUp)
-$dbUp = [StandardExtensions]::LogToConsole($dbUp)
+$dbUp = [StandardExtensions]::LogTo($dbUp, (New-Object VstsUpgradeLog))
 if ($Journal -eq "NullJournal") {
     $dbUp = [StandardExtensions]::JournalTo($dbUp, (New-Object DbUp.Helpers.NullJournal))
 }
 else {
     $dbUp = [SqlServerExtensions]::JournalToSqlTable($dbUp, 'dbo', '_SchemaVersions')
 }
-$dbUp.Build().PerformUpgrade() | Out-Null
+$result = $dbUp.Build().PerformUpgrade()
+if (!$result.Successful) {
+    $errorMessage = ""
+    if ($result.Error -ne $null) {
+        $errorMessage = $result.Error.Message
+    }
+    Write-Host "##vso[task.logissue type=error;]Database migration failed. $errorMessage"
+    Write-Host "##vso[task.complete result=Failed;]"
+    Exit -1
+}
