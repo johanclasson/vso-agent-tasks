67664382350ffa8dba514883f8a6fc533279607f
Johan Classon <johan.classon@nethouse.se>
2016-11-21 10:50:44 +0100

    Update RoboCopy.ps1

--- RoboCopy/RoboCopy.ps1
+++ RoboCopy/RoboCopy.ps1
@@ -1,6 +1,6 @@
 param([string]$Source, [string]$Destination)
 
-robocopy /MIR /R:5 /W:30 $Source $Destination
+robocopy /MIR /R:5 /W:30 /NFL /NDL "$Source" "$Destination"
 if ($LASTEXITCODE -gt 3){
     Write-Error "Something bad happened ($LASTEXITCODE)"
     exit -1
